7e1f2fe06866afca8da55d8e521eb9c3a077c1d2
Justyna Foster <justynawfoster@gmail.com>
2017-04-03 16:35:08 -0600

    solve marge issues

--- public/js/user.js
+++ public/js/user.js
@@ -1,20 +1,11 @@
 'use strict';
-<<<<<<< HEAD
 $( 'document' ).ready( function() {
-=======
-console.log( "Getting to users.js" );
-$( 'document' ).ready( function() {
-  console.log( "Let's explore!" );
->>>>>>> cf5ee061d0c3fbcf55bfd395ac09deff67fc2f26
 
   $.getJSON( '/user_town_lists/false' )
     .done( ( town ) => {
       var card = town;
       card.map( renderCardsWant );
-<<<<<<< HEAD
-=======
-      console.log( town );
->>>>>>> cf5ee061d0c3fbcf55bfd395ac09deff67fc2f26
+
     } )
     .fail( function( err ) {
       console.log( err );
@@ -27,13 +18,8 @@ $( 'document' ).ready( function() {
     this.townPhoto = obj[ "photo_url" ];
     this.townName = obj[ "name" ];
     this.townShortDesc = obj[ "short_desc" ];
-<<<<<<< HEAD
   }
 
-=======
-    console.log( "I'm getting to line 25" );
-  }
->>>>>>> cf5ee061d0c3fbcf55bfd395ac09deff67fc2f26
   UserTownWant.prototype.populateCard = function populateCard() {
     var townPhoto = "<div class='col s3'>" + "<div class='card small'>" + "<div class='card-image'>" + "<img src= '" + this.townPhoto + "' alt= '" + this.townName + "' class='responsive-img imageSizing'>";
     var townName = "<span class='card-title center-align'>" + this.townName + "</span>" + "</div>";
@@ -55,10 +41,6 @@ $( 'document' ).ready( function() {
     .done( ( town ) => {
       var card = town;
       card.map( renderCardsBeen );
-<<<<<<< HEAD
-=======
-      console.log( town );
->>>>>>> cf5ee061d0c3fbcf55bfd395ac09deff67fc2f26
     } )
     .fail( function( err ) {
       console.log( err );
@@ -72,13 +54,9 @@ $( 'document' ).ready( function() {
     this.townPhoto = obj[ "photo_url" ];
     this.townName = obj[ "name" ];
     this.townShortDesc = obj[ "short_desc" ];
-<<<<<<< HEAD
   }
 
-=======
-    console.log( "I'm getting to line 25" );
-  }
->>>>>>> cf5ee061d0c3fbcf55bfd395ac09deff67fc2f26
+
   UserTownBeen.prototype.populateCard = function populateCard() {
     var townPhoto = "<div class='col s3'>" + "<div class='card small'>" + "<div class='card-image'>" + "<img src= '" + this.townPhoto + "' alt= '" + this.townName + "' class='responsive-img imageSizing'>";
     var townName = "<span class='card-title center-align'>" + this.townName + "</span>" + "</div>";
@@ -93,19 +71,9 @@ $( 'document' ).ready( function() {
     newCard.populateCard();
   }
 
-<<<<<<< HEAD
   // ====Javascript rendering for info box====
   $.getJSON( '/users/getid' )
     .done( ( user ) => {
-=======
-
-
-
-  // ====Javascript rendering for info box====
-  $.getJSON( '/users/getid' )
-    .done( ( user ) => {
-      console.log( "user: ", user.data );
->>>>>>> cf5ee061d0c3fbcf55bfd395ac09deff67fc2f26
       const html = "<p class='center-align'>" + user.data.username + "</p>" + "<p class='center-align'>" + user.data.location_city + "</p>" + "<p class='center-align'>" + user.data.location_state + "</p>";
       $( '#profileInfoBox' ).append( html );
     } )
@@ -119,31 +87,18 @@ $( 'document' ).ready( function() {
     this.username = obj[ "username" ];
     this.location_city = obj[ "location_city" ];
     this.location_state = obj[ "location_state" ];
-<<<<<<< HEAD
   }
 
-=======
-    console.log( obj );
-  }
->>>>>>> cf5ee061d0c3fbcf55bfd395ac09deff67fc2f26
   // render to user info box
   UserInfo.prototype.populateInfoBox = function populateInfoBox() {
     const html = "<p class='center-align'>" + this.username + "</p>" + "<p class='center-align'>" + this.location_city + "</p>" + "<p class'center-align'>" + this.location_state + "</p>";
     $( '.profileInfoBox' ).html( html );
-<<<<<<< HEAD
-=======
-    console.log( html );
->>>>>>> cf5ee061d0c3fbcf55bfd395ac09deff67fc2f26
+
   };
 
   function renderUserInfo( obj ) {
     var newUser = new UserInfo( obj );
     newUser.populateInfoBox();
   }
-<<<<<<< HEAD
 });
-=======
 
-  // })
-} );
->>>>>>> cf5ee061d0c3fbcf55bfd395ac09deff67fc2f26
